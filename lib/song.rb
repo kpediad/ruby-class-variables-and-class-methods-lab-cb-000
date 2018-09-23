a20ada21c7cc98511a7c851acfe2e62e34bca93b
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-09-23 12:39:49 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,19 @@ class Song
   attr_accessor :name, :artist, :genre
 
   @@count = 0
-
+  @@genres = []
+  
   def initialize(name, artist, genre)
     @@count += 1
-    
+    @@genres << genre unless @@genres.include?(genre)
   end
 
   def self.count
     @@count
+  end
+
+  def self.genres
+    @@genres
   end
   
 end
